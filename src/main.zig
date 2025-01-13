a3adb0c9ef1a23d0befd24763e1b0f49b92a60b8
walltime <wa@ll.time>
2025-01-13 19:42:42 +0200

    added comments for main

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const testing = std.testing;
 
+const printError = error{notPrintable};
+
 pub fn main() !void {
     // read filename
 
@@ -29,21 +31,61 @@ pub fn main() !void {
 
     const lineStop: u8 = 0x10;
 
-    std.debug.print("{x:0>8}: ", .{fileSize});
+    std.debug.print("@hexdump: ", .{});
     for (0..lineStop) |i| {
         std.debug.print("{x:0>2} ", .{i});
     }
     std.debug.print("\n", .{});
 
+    // main file read loop
+    // in order to achieve true hexdump i need to
+    // create a string and populate it with
+    // possible printable characters
+
+    var line: [lineStop]u8 = [_]u8{'.'} ** lineStop;
+    // var content: [64]u8 = [_]u8{'A'} ** 64;
+
+    // loop to the end of the file size
     for (0..fileSize) |i| {
+
+        // read one character
         const char = buffer[i];
 
+        // when we reach the end of a line
         if (i % lineStop == 0) {
-            if (i != 0x0) std.debug.print("\n", .{});
+
+            // but not the end of the first line
+            // print the printable line and a newline
+            if (i != 0x0) {
+                std.debug.print("| {s}\n", .{line});
+                line = [_]u8{'.'} ** lineStop;
+            }
+
+            // print the start of a new line and its offest
             std.debug.print("{x:0>8}: ", .{i});
+            // clear the printable line
         }
+
+        // if char is printable add it to the end string
+        line[i % lineStop] = printable(char) catch '.';
+
+        // print a char hex
         std.debug.print("{x:0>2} ", .{char});
+
+        if (i == fileSize - 1) {
+            for ((i % lineStop)..lineStop - 1) |_| {
+                std.debug.print("   ", .{});
+            }
+            std.debug.print("| {s}\n", .{line});
+        }
     }
+}
+
+fn printable(char: u8) !u8 {
+    // if char in range return char
+    // else return error
+    if (char > 31 and char != 127) return char;
+    return printError.notPrintable;
 }
 
 //alternative allocator
@@ -73,4 +115,15 @@ test "modulo" {
     // if we pass a zero as denominator.
     // But which error needs to be tested?
     // try testing.expectError(error.DivisionByZero, divide(15, 0));
+}
+
+test "load_string" {
+    const lineStop = 2;
+    const i = 0;
+    const char: u8 = 't';
+    var line: [lineStop]u8 = [_]u8{'.'} ** lineStop;
+
+    line[i % lineStop] = printable(char) catch '.';
+
+    try testing.expect(line[0] == char);
 }
